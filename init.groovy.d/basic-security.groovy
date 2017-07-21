90cd44f6957263f395a9eff9bbe07ce416207c28
R. Tyler Croy <tyler@monkeypox.org>
2017-07-20 21:18:20 -0700

    We'll have one groovy script per function, not one uber-script

--- init.groovy.d/basic-security.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env groovy
-/*
- * Set up the basic levels of authorization and other security settings which
- * should be defaulted in 2.0, but are not in our instances because we're
- * skipping the setup wizard
- */
