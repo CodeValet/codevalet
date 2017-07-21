e4c32fbf0c0f07e9f2ddaf6e8593e5c8dab0584a
R. Tyler Croy <tyler@monkeypox.org>
2017-07-20 17:18:25 -0700

    Starting to prepare the basic init.groovy.d/ scripts necessary for the masters

---
+++ jenkins-bootstrap/enable-csrf-protection.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+/*
+ * Set up the CSRF protection which would normally be defaulted in 2.0 , but
+ * are not in our instances because we're * skipping the setup wizard
+ */
+
+import jenkins.model.*
+import hudson.security.csrf.*
+
+println "Checking CSRF protection..."
+if (instance.crumbIssuer == null) {
+  println "Enabling CSRF protection"
+  instance.crumbIssuer = new DefaultCrumbIssuer(true)
+}
