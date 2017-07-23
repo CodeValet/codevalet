2d3d417eed9dbf8393ed70c769e55a119ed15106
R. Tyler Croy <tyler@monkeypox.org>
2017-07-23 15:05:44 -0700

    Secrets are coming out of Kubernetes with an annoying trailing newline
    Which predictably busts the hashing necessary to make OAuth tokens

--- webapp/app.rb
+++ webapp/app.rb
@@ -30,7 +30,7 @@ module CodeValet
       config.scope_defaults :default, :config => {
         :scope            => 'read:org,user:email',
         :client_id        => ENV['GITHUB_CLIENT_ID'] || 'a6f2001b9e6c3fabf85c',
-        :client_secret    => ENV['GITHUB_CLIENT_SECRET'] || '0672e14addb9f41dec11b5da1219017edfc82a58',
+        :client_secret    => (ENV['GITHUB_CLIENT_SECRET'] || '0672e14addb9f41dec11b5da1219017edfc82a58').chomp,
         :redirect_uri     => ENV['REDIRECT_URI'] || 'http://localhost:9292/github/authenticate',
       }
 
