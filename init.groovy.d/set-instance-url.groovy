a9ead77b788b2ad852bead22a8029bf4ae870fa4
R. Tyler Croy <tyler@monkeypox.org>
2017-07-26 19:13:07 -0700

    Set up the instance URL

---
+++ init.groovy.d/set-instance-url.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+/* Configure the instance's URL based on the GITHUB_USER */
+
+import jenkins.model.*
+
+if (System.env.get('GITHUB_USER')) {
+    JenkinsLocationConfiguration.get().setUrl("http://${System.env.get('GITHUB_USER')}.codevalet.io/")
+}
