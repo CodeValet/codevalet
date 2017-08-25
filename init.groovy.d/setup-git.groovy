e57338aedbed67f0311dc16056d348e93598e09e
R. Tyler Croy <tyler@monkeypox.org>
2017-08-24 19:42:30 -0700

    Properly configure JGit as the default tool
    I HAVE NO IDEA WHAT IS GOING ON HERE.

--- init.groovy.d/setup-git.groovy
+++ init.groovy.d/setup-git.groovy
@@ -3,12 +3,17 @@
  * Pre-configure the Git plugin and the default git tooling in Jenkins
  */
 import org.jenkinsci.plugins.gitclient.*
+import hudson.plugins.git.*
 import jenkins.model.Jenkins
 
 def gitConfig = Jenkins.instance.getDescriptor('hudson.plugins.git.GitSCM')
 def tools = Jenkins.instance.getDescriptor('hudson.plugins.git.GitTool')
 
-tools.setInstallations(new JGitTool())
+GitTool[] gitTools = new GitTool[1]
+gitTools[0] = new JGitTool()
+
+tools.installations = gitTools
+tools.save()
 
 gitConfig.globalConfigName = 'max'
 gitConfig.globalConfigEmail = 'max@example.com'
