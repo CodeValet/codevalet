b23f9bea3e3e2ea0280dd99f3548ee6bdbc1048c
R. Tyler Croy <tyler@monkeypox.org>
2017-08-26 00:47:42 -0700

    Add datadog integration into the master infrastructure

---
+++ init.groovy.d/configure-datadog.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy 
+
+/* Configure the Datadog plugin for this instance */
+
+import jenkins.model.*
+import org.datadog.jenkins.plugins.datadog.DatadogBuildListener
+
+def dog = Jenkins.instance.getDescriptor('org.datadog.jenkins.plugins.datadog.DatadogBuildListener')
+
+if (System.env.get('GITHUB_USER')) {
+    dog.hostname = "http://${System.env.get('GITHUB_USER')}.codevalet.io/"
+}
+
+dog.tagNode = true
+dog.apiKey = System.env.get('DATADOG_API_KEY') ?: 'dummy-datadog-api-key'
+
+dog.save()
