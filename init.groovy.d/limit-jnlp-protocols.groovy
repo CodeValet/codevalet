3b8a926b4a951dde767832cef6a9fda5ce687065
R. Tyler Croy <tyler@monkeypox.org>
2017-08-26 02:17:47 -0700

    Enable Agent->Master Access Control

--- init.groovy.d/limit-jnlp-protocols.groovy
+++ init.groovy.d/limit-jnlp-protocols.groovy
@@ -5,6 +5,10 @@
  */
 
 import jenkins.model.*
+import org.kohsuke.stapler.StaplerProxy
+import jenkins.security.s2m.AdminWhitelistRule
 
 Jenkins.instance.agentProtocols = ['JNLP4-connect', 'Ping']
 Jenkins.instance.save()
+
+Jenkins.instance.getExtensionList(StaplerProxy).get(AdminWhitelistRule).masterKillSwitch = false
