38d99c471827c72602bc3d37506dddd8d22a4dbb
R. Tyler Croy <tyler@monkeypox.org>
2017-11-29 17:36:18 -0800

    Add an Azure Container Registry to the infra plane
    This is not yet being utilized by AKS, but should be

--- plans/control-plane.tf
+++ plans/control-plane.tf
@@ -8,3 +8,19 @@ resource "azurerm_resource_group" "controlplane" {
     name     = "codevalet"
     location = "${var.region}"
 }
+
+resource "azurerm_container_registry" "registry" {
+    name                = "codevalet"
+    resource_group_name = "${azurerm_resource_group.controlplane.name}"
+    location            = "${azurerm_resource_group.controlplane.location}"
+    admin_enabled       = true
+    sku                 = "Standard"
+}
+
+resource "azurerm_storage_account" "storage" {
+    name                     = "codevaletstorage"
+    resource_group_name      = "${azurerm_resource_group.controlplane.name}"
+    location                 = "${azurerm_resource_group.controlplane.location}"
+    account_tier             = "Standard"
+    account_replication_type = "GRS"
+}
